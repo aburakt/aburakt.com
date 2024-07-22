b576039f0b4000b178953856a54abdedf2413c16
aburakt <info@aburakt.com>
2024-07-22 15:32:32 +0300

    fix: vercel deploy error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import IframeComponent from "./components/IframeComponent"
 
 function App() {
   return (
     <>
+      <SpeedInsights />
+      <Analytics />
       <IframeComponent />
     </>
   )
