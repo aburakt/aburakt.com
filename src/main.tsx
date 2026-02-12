6a04ebb740dd81737fd576ac7bfdbd3d375df787
aburakt <info@aburakt.com>
2026-02-12 12:57:24 +0000

    fix(router): support /jawu base path via BrowserRouter basename

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './styles/tailwind.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </HelmetProvider>
